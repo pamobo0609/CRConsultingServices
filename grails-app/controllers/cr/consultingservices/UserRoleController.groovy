63a0ac7fd7c2319e2d3d6a4c292c8c6b14c75180
pamobo0609 <jpmonge0609@gmail.com>
2016-03-07 18:05:29 -0600

    Fixes MySQL error
    Fixed error, missing login and logout controllers

---
+++ grails-app/controllers/cr/consultingservices/UserRoleController.groovy
@@ -0,0 +1,108 @@
+package cr.consultingservices
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+import grails.plugin.springsecurity.annotation.Secured
+
+@Transactional(readOnly = true)
+@Secured('permitAll')
+class UserRoleController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond UserRole.list(params), model:[userRoleInstanceCount: UserRole.count()]
+    }
+
+    def show(UserRole userRoleInstance) {
+        respond userRoleInstance
+    }
+
+    def create() {
+        respond new UserRole(params)
+    }
+
+    @Transactional
+    def save(UserRole userRoleInstance) {
+        if (userRoleInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userRoleInstance.hasErrors()) {
+            respond userRoleInstance.errors, view:'create'
+            return
+        }
+
+		if (userRoleInstance ==  null)
+			userRoleInstance = new UserRole(params)
+		
+        userRoleInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'userRole.label', default: 'UserRole'), userRoleInstance.id])
+                //redirect userRoleInstance
+				redirect action:'show/${userRoleInstance?.id}', method: "GET"
+            }
+            '*' { respond userRoleInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(UserRole userRoleInstance) {
+        respond userRoleInstance
+    }
+
+    @Transactional
+    def update(UserRole userRoleInstance) {
+        if (userRoleInstance == null) {
+            notFound()
+            return
+        }
+
+        if (userRoleInstance.hasErrors()) {
+            respond userRoleInstance.errors, view:'edit'
+            return
+        }
+
+        userRoleInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'UserRole.label', default: 'UserRole'), userRoleInstance.id])
+                redirect userRoleInstance
+            }
+            '*'{ respond userRoleInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(UserRole userRoleInstance) {
+
+        if (userRoleInstance == null) {
+            notFound()
+            return
+        }
+
+        userRoleInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'UserRole.label', default: 'UserRole'), userRoleInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'userRole.label', default: 'UserRole'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
